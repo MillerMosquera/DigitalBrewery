a346084b6c056ee5c14a8c0e71491fc2f0acd40f
Miller Mosquera <millerpalacios18@hotmail.com>
2024-10-15 10:57:17 -0500

    Squashed commit of the following:
    commit b91d2a0c88a063d647500c48a09ab9011317d1ff
    Author: Miller Mosquera <millerpalacios18@hotmail.com>
    Date:   Mon Oct 14 23:13:28 2024 -0500
    
        fix(proyecto): actualiza el proyecto
    
    commit 89584dba78f21d3be86d968f9cd0e1a5b4885953
    Author: Miller Mosquera <millerpalacios18@hotmail.com>
    Date:   Mon Oct 14 22:44:50 2024 -0500
    
        feat(modelo): agregado entorno básico cervecería
    
    commit cf1e75f9eb78aa02148b6245428a44fa0fb686b8
    Merge: d9c92cb 6ed67c9
    Author: Miller Mosquera <millerpalacios18@hotmail.com>
    Date:   Wed Oct 9 22:14:18 2024 -0500
    
        Merge branch 'master' of https://github.com/MillerMosquera/DigitalBrewery
    
    commit d9c92cbce58a9143df629d020cb4878a5f10da41
    Merge: a504d36 583ca02
    Author: Miller Mosquera <44532720+MillerMosquera@users.noreply.github.com>
    Date:   Wed Oct 9 19:13:07 2024 -0500
    
        fix(ui): corrige errores en menu principal para iniciar el gemelo digital
    
    commit 6ed67c93e3b9a2bbfc0c371375ea33491287a406
    Merge: a504d36 583ca02
    Author: Miller Mosquera <44532720+MillerMosquera@users.noreply.github.com>
    Date:   Wed Oct 9 19:13:07 2024 -0500
    
        Merge pull request #1 from MillerMosquera/miller
    
        feat(ui): agrega menu principal para iniciar el gemelo digital
    
    commit 583ca02835ab8f084b3428f35aa4c698d7270043
    Author: Miller Mosquera <millerpalacios18@hotmail.com>
    Date:   Wed Oct 9 18:58:18 2024 -0500
    
        feat(ui): agrega menu principal para iniciar el gemelo digital

--- Brewery3D/Intermediate/Build/Win64/x64/Brewery3DEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++ Brewery3D/Intermediate/Build/Win64/x64/Brewery3DEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/Miller/Desktop/DigitalBrewery/Brewery3D/Intermediate/Build/Win64/x64/Brewery3DEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "C:/Proyectos/DigitalBrewery/Brewery3D/Intermediate/Build/Win64/x64/Brewery3DEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
 #include "Runtime/Core/Public/CoreSharedPCH.h"
